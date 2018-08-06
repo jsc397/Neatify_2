97633c1cd0ca69a53d242fdaa7e3a9453542a3bb
“Stephanie <stephanie.techarmour@gmail.com>
2018-08-06 17:27:28 -0400

    fixed path for css and background image works. now saying that we don't need to specificy public everytime

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
-const hbs = require('hbs')
+const hbs = require("hbs")
 const bodyParser = require('body-parser')
-const appController = require('../controllers/appController')
-
 const app = express()
+const path = require('path')
+
 app.set("view engine", "hbs")
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('public/images')) //make images folder to use this
-app.use(express.static('public/css')) //link to css folder in public
-// app.use()
+
+app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.use(require('./routes/index.js'))
 
 app.listen(2000, () => console.log("Locked and loaded on port 2000"))
