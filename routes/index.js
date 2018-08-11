015c26b200ebb6f532fe7b5b21f3da067a0f11b9
“Stephanie <stephanie.techarmour@gmail.com>
2018-08-11 17:46:05 -0400

    when you submit solution from problem show page, goes to the user show page

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,11 @@ const applicationController = require("../controllers/application.js")
 router.use('/user', require('./user'))
 // router.use('/solutions', require('./solution'))
 router.use('/problems', require('./problems'))
+router.use('/', require("./application.js"))
 router.use('/', applicationController.index)
 
-// router.all('*',(req,res)=> {
-//     res.status(400).send()
-// })
+router.all('*',(req,res)=> {
+    res.status(400).send()
+})
 
 module.exports = router
