3e35a1e427439f862bab886e80f9b977557fe8d8
“Stephanie <stephanie.techarmour@gmail.com>
2018-08-14 17:33:32 -0400

    commit before wonky routes commence

--- controllers/application.js
+++ controllers/application.js
@@ -3,8 +3,5 @@ module.exports = {
     index : (req, res) => {
         res.render("app/index")
     }
-    // show: (req, res) => {
-    //     Problem.findOne({_id: req.params.id}) 
-    //     .then(console.log(req))       
-    // }
+
 }
--- routes/solution.js
+++ routes/solution.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const solutionController = require("../controllers/solution")
 
 //do I need to show the individual solution? so from my problems index page, which lists all of my problems, I want to be able to click the problem, which will go to its individual page, populate the problem and user can create his/her own solution to the specific problem from the problem/:id page.
-router.get("/", solutionController.new)
-router.post("/", solutionController.create)
+router.get("/problem/:id", solutionController.new)
+router.post("/problem/:id", solutionController.create)
 // router.get("/:id",solutionController.show)
 // router.get("/:id",solutionController.update)
 
