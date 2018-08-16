308a12dadde75bbc782877aefef800fa484891c8
“Stephanie <stephanie.techarmour@gmail.com>
2018-08-16 19:16:08 -0400

    update for problem works but need to work on where to send it and save the same solution to user solution page

--- controllers/problems.js
+++ controllers/problems.js
@@ -33,57 +33,11 @@ module.exports = {
                 problem.solutions.push(solution)
                 // save problem with new solution
                 problem.save(err => {
-                    // redirect back to problem show page
-                    res.redirect(`/user/${user._id}`)
+                    // redirect back to problem show page..actual code to update.
+                    // send this to solution to user show page and save it to
+                    res.redirect(`/`)
                 })
             })
         })
     }
 }
-
-
-    // update: (req, res) => {
-    //     let { content } = req.body
-    //     Problem.findOne({_id: req.params.id})
-    //     .then(problem => {
-    //         problem.solutions.push({
-    //              content
-    //             });
-    //             problem.save(err => {
-    //                 res.redirect('/')
-    //              })
-    //             })
-    //         // console.log(req.body)
-    //     }
-    
-
-        // create:  (req, res) => {
-        //     console.log(req.body)
-        //     Solution.create({
-        //     content: req.body.solutions.content,
-        //      author: req.user._id
-        //      }).then(solution => {
-        //         User.findOne({_id: req.body.author})
-        //         .then(user => {
-        //          user.solutions.push(solution);
-        //         user.save(err => {
-        //         res.redirect(`/user/${user._id}`)
-        //             })
-        //         })
-        //      })
-        // },
-
-
-        // show
-                // .populate("author")
-                // .populate("comments.author")
-                // .then(gif => {
-                //     res.render('gif/show', gif) })
-
-                // Problem.findOne({ _id: req.params.id })
-                //     .populate("solutions")
-                //     .then(solutions => {
-                //         res.render('problem/show', solutions)
-                //     })
-                // },
-                //  res.send('problems show view')
