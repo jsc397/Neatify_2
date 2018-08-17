91036aa1c7cbd8ed6171430bed831de71db5dda1
“Stephanie <stephanie.techarmour@gmail.com>
2018-08-17 14:13:55 -0400

    working on pushing solution to user page

--- controllers/problems.js
+++ controllers/problems.js
@@ -21,23 +21,52 @@ module.exports = {
     },
    update: (req, res) => {
     let { content } = req.body
-     // create a new solution based on content
-        Solution.create({ 
-            content: content
-        }).then(solution => {
-        // find problem by it's ID
-            Problem.findOne({
-                _id: req.params.id
-            }).then(problem => {
-                // push solution to problem.solutions
-                problem.solutions.push(solution)
-                // save problem with new solution
-                problem.save(err => {
-                    // redirect back to problem show page..actual code to update.
-                    // send this to solution to user show page and save it to
-                    res.redirect(`/`)
-                })
-            })
+    console.log(req)
+    // create a new solution based on content
+       Solution.create({
+           content: content,
+           author: req.user._id
+       }).then(solution => {
+    // find problem by it's ID
+        Problem.findOne({
+            _id: req.body.id
         })
-    }
+       })
+    // push solution to problem.solutions
+                
+    // save problem with new solution
+              
+    // find User by USERID
+
+    //push the saved solution to the User solution array
+   
+                
+            
+        
+    
 }
+
+
+// {
+//     let { content } = req.body
+//     // create a new solution based on content
+//     Solution.create({
+//         content: content
+//     }).then(solution => {
+//         // find problem by it's ID
+//         Problem.findOne({
+//             _id: req.params.id
+//         }).then(problem => {
+//             // push solution to problem.solutions
+//             problem.solutions.push(solution)
+//             // save problem with new solution
+//             problem.save(err => {
+//                 // redirect back to problem show page..actual code to update.
+//                 // send this to solution to user show page and save it to
+//                 res.redirect(`/`)
+//             })
+//         })
+//     })
+// }
+
+//save problem with new solutions
