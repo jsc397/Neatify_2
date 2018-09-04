ce5bfab54b8d6f2cd6ff23182caa5b3e8b86701e
“Stephanie <stephanie.techarmour@gmail.com>
2018-09-04 19:27:13 -0400

    added Procfile and set up for remote deployment

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,12 @@
-const mongoose = require('mongoose')
-mongoose.connect("mongodb://localhost:27017/neatify_2", { useNewUrlParser: true });
-mongoose.Promise = Promise
-module.exports = mongoose
+// mongoose.connect('mongodb://localhost/db_name')
+const mongoose = require("mongoose");
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL);
+} else {
+  mongoose.connect(
+    "mongodb://localhost:27017/neatify_2",
+    { useNewUrlParser: true }
+  ); //dangerzone is our local database name;
+}
+mongoose.Promise = Promise;
+module.exports = mongoose;
