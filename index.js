50743ff0a1fd6cd83ab4e5e37bbd24a2ca7189f3
“Stephanie <stephanie.techarmour@gmail.com>
2018-09-04 19:40:24 -0400

    changed server name to index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(require("./routes/index.js"));
 
-app.set("port", process.env.PORT || 2001);
+app.set("port", process.env.PORT || 3001);
 
 app.listen(app.get("port"), () => {
   console.log(`✅ PORT: ${app.get("port")} 🌟`);
