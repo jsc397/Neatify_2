c21f42a0b4f7ec7e2b30e77b40570aed44f78278
“Stephanie <stephanie.techarmour@gmail.com>
2018-09-04 23:00:48 -0400

    second seed data to test

---
+++ db/seeds.js
@@ -0,0 +1,28 @@
+const User = require("../models/User");
+const { Solution } = require("../models/Solution");
+const Problem = require("../models/Problem");
+
+Problem.find({}).remove(() => {
+  Solution.find({}).remove(() => {
+    Problem.create({
+      paragraph: "This is problem number1."
+    }).then(problem => {
+      Promise.all([
+        Solution.create({
+          content: "this is problem1 attempted solution",
+          author: user._id
+        }).then(solution => {
+          problem.solutions.push(solution);
+        }),
+        Solution.create({
+          content: "This is another solution to problem1",
+          author: user._id
+        }).then(solution => {
+          problem.solutions.push(solution);
+        })
+      ]).then(() => {
+        problem.save(err => console.log(err));
+      });
+    });
+  });
+});
