01c93e73945505f8e7bc55b9ad712d84bb3c1005
“Stephanie <stephanie.techarmour@gmail.com>
2018-09-26 01:02:20 -0400

    cleaned up server.js

--- server.js
+++ server.js
@@ -2,16 +2,10 @@ const express = require("express");
 const hbs = require("hbs");
 const bodyParser = require("body-parser");
 const app = express();
-const path = require("path");
 const methodOverride = require("method-override");
 
 app.set("view engine", "hbs");
 app.use(express.static("public"));
-// app.use(express.static(path.join(__dirname, "public"))); //  "public" off of current is root
-
-// app.use(express.static(path.join(__dirname, "/public")));
-// app.use("/static", express.static(path.join(__dirname, "public")));
-hbs.registerPartials(__dirname + "/views/partials");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(require("./routes/index.js"));
